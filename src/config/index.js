796aca3a3382da7e928c33d3841155d5be7abed8
陈桥格 <qiaoge.chen@ikang.com>
2020-06-30 17:45:42 +0800

    change

--- src/config/index.js
+++ src/config/index.js
@@ -5,8 +5,8 @@ export default {
   host: '', // 初始地址
   // 基准地址表
   hostServe: {
-    test: 'http://test.vmall.ikang.com/v4',
-    uat: 'http://uat.vmall.ikang.com/v4',
+    test: 'http://test.com/v4',
+    uat: 'http://uat.com/v4',
     pre: '/v4',
     prod: '/v4'
   },
